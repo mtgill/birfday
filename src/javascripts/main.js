0d4d77091c836400102d1532d0a7ddb49c3f3e84
Matt Gill <mtgill90@outlook.com>
2019-05-20 21:26:53 -0500

    auth working

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,11 +3,15 @@ import 'bootstrap';
 import '../styles/main.scss';
 import auth from './components/auth/auth';
 import birfday from './components/birfday/birfday';
+import MyNavBar from './components/MyNavbar/MyNavbar';
+import authData from './helpers/data/authData';
 
 import apiKeys from './helpers/apiKeys.json';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  MyNavBar.navbarEvents();
+  authData.checkLoginStatus();
   auth.authStringBuilder();
   birfday.birfdayStringBuilder();
 };
