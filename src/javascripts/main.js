cf63cbc255f4c661421b120978792aafdf2190b7
Matt Gill <mtgill90@outlook.com>
2019-05-20 19:55:12 -0500

    initialized firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,13 @@
+import firebase from 'firebase/app';
 import 'bootstrap';
 import '../styles/main.scss';
 import auth from './components/auth/auth';
 import birfday from './components/birfday/birfday';
 
+import apiKeys from './helpers/apiKeys.json';
+
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   auth.authStringBuilder();
   birfday.birfdayStringBuilder();
 };
