9246d515d5081f6afde12b2c863ad6348249d463
Matt Gill <mtgill90@outlook.com>
2019-05-25 11:30:11 -0500

    birthdays printing to dom

--- src/javascripts/components/birfday/birfday.js
+++ src/javascripts/components/birfday/birfday.js
@@ -1,9 +1,15 @@
 import util from '../../helpers/util';
+import birfdayData from '../../helpers/data/birfdayData';
 
-const birfdayStringBuilder = () => {
-  let domString = '';
-  domString += '<h2>Birfday</h2>';
-  util.printToDom('birfday', domString);
+const birfdayStringBuilder = (uid) => {
+  birfdayData.getBirfdayByUid(uid).then((birthday) => {
+    let domString = '';
+    domString += `<h1>${birthday.date}</h1>`;
+    domString += `<img src="${birthday.imageUrl}" alt="birthday location" />`;
+    domString += `<h2>${birthday.location} @ ${birthday.time}</h2>`;
+    util.printToDom('event', domString);
+  })
+    .catch(err => console.error('could not get birfday', err));
 };
 
 export default { birfdayStringBuilder };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,7 +2,6 @@ import firebase from 'firebase/app';
 import 'bootstrap';
 import '../styles/main.scss';
 import auth from './components/auth/auth';
-import birfday from './components/birfday/birfday';
 import MyNavBar from './components/MyNavbar/MyNavbar';
 import authData from './helpers/data/authData';
 
@@ -13,7 +12,6 @@ const init = () => {
   MyNavBar.navbarEvents();
   authData.checkLoginStatus();
   auth.authStringBuilder();
-  birfday.birfdayStringBuilder();
 };
 
 init();
